3e1f1b98d3feb5098c9756af75e21934af65cc61
Shahjalal56 <Shahzalalkhan@gmail.com>
2025-10-13 11:39:50 +0600

    Add invoice screen ui

--- lib/presentation/view/admin_flow/admin/bottom_nav/screen/wrapper.dart
+++ lib/presentation/view/admin_flow/admin/bottom_nav/screen/wrapper.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:hmlegends/core/constant/app_colors.dart';
 import 'package:hmlegends/presentation/view_model/parent/bottom_nav_viewmodel.dart';
 import 'package:provider/provider.dart';
+import '../../../../branch_manager_flow/Invoice/Invoice_screen.dart';
 import '../../home/screen/head_office_home_screen.dart';
+import '../../invoice/screen/head_office_invoice_screen.dart';
 import '../../order/screen/order_summary_screen.dart';
 import '../../stock/screen/stock_screen.dart';
 import '../widget/custom_bottom_nav_bar.dart';
@@ -16,7 +18,7 @@ class MainWrapper extends StatelessWidget {
       const HeadOfficeHomeScreen(),
       StockScreen(),
       OrderSummaryScreen(),
-      const HeadOfficeHomeScreen(),
+      HeadOfficeInvoiceScreen(),
     ];
 
     return Consumer<BottomNavViewModel>(
